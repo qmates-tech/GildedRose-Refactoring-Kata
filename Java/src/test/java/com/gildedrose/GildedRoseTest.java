1d994eaa664db39b17f5bf7512b80f4061e6f3a8
Daniele Megna <megna.dany@gmail.com>
2023-08-29 11:18:58 +0200

    Starting with characterization tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void initAppWithAnEmptyItemsArray() {
+        GildedRose app = new GildedRose(new Item[] {});
+        assertEquals(0, app.items.length);
+
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(0, app.items.length);
     }
 
+    @Test
+    void initAppWithASingleItem() {
+        Item[] items = new Item[]{new Item("Item name", 4, 3)};
+        GildedRose app = new GildedRose(items);
+
+        assertEquals(1, app.items.length);
+        Item appItem = app.items[0];
+        assertEquals("Item name", appItem.name);
+        assertEquals(3, appItem.quality);
+        assertEquals(4, appItem.sellIn);
+    }
+
+    @Test
+    void qualityAndSellInDecreaseByOneEveryday() {
+        Item updated = updateToNextDay(new Item("Item name", 4, 3));
+        assertEquals(2, updated.quality);
+        assertEquals(3, updated.sellIn);
+
+        updated = updateToNextDay(updated);
+        assertEquals(1, updated.quality);
+        assertEquals(2, updated.sellIn);
+    }
+
+    @Test
+    void qualityOfAnItemIsNeverNegative() {
+        Item updated = updateToNextDay(new Item("Item name", 0, 0));
+        assertEquals(0, updated.quality);
+    }
+
+    @Test
+    void sellInValueCanBeNegative() {
+        Item updated = updateToNextDay(new Item("Item name", 0, 0));
+        assertEquals(-1, updated.sellIn);
+    }
+
+    private static Item updateToNextDay(Item item) {
+        GildedRose app = new GildedRose(new Item[] {item});
+        app.updateQuality();
+        return app.items[0];
+    }
 }
